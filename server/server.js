21fcb8e47dbdc34ce510a50d832a8cfdc82e10c7
JacobGain <jgain@uwo.ca>
2024-10-31 17:10:03 -0400

    Item 1 functionality, get all info of a given ID

--- server/server.js
+++ server/server.js
@@ -1,13 +1,53 @@
 const fs = require("fs");
 const csvParser = require("csv-parser");
+const express = require("express");
+const app = express();
 
+app.use(express.json())
 
 const destinationsJSON = [] // stores the csv data in JSON format
 fs.createReadStream("data/europe-destinations.csv")
     .pipe(csvParser())
     .on("data", (data) => {
-        destinationsJSON.push(data);
+        const cleanData = Object.keys(data).reduce((acc, key) => {
+            const cleaningKey = key.replace(/[^\x00-\x7F]/g, '').trim();
+            acc[cleaningKey] = data[key];
+            return acc;
+        }, {});
+        destinationsJSON.push(cleanData);
     })
     .on("end", () => {
-        console.log(destinationsJSON);
+        //console.log(destinationsJSON);
     });
+
+// item 1, get all information of a given destination ID
+app.get('/api/destinations/:id', (req, res) => {
+    const destination = destinationsJSON[req.params.id - 1]
+    const destinationInfo = { 
+        Destination: destination["Destination"],
+        Region: destination["Region"],
+        Country: destination["Country"],
+        Category: destination["Category"],
+        Latitude: destination["Latitude"],
+        Longitude: destination["Longitude"],
+        ApproxAnnualTourists: destination["Approximate Annual Tourists"],
+        Currency: destination["Currency"],
+        MajorityReligion: destination["Majority Religion"],
+        FamousFoods: destination["Famous Foods"],
+        Language: destination["Language"],
+        BestTimeToVisit: destination["Best Time to Visit"],
+        CostOfLiving: destination["Cost of Living"],
+        Safety: destination["Safety"],
+        CulturalSignificance: destination["Cultural Significance"],
+        Description: destination["Description"]
+    };
+
+    res.send(destinationInfo)
+
+});
+
+
+
+// PORT environment variable, part of enviro where process runs
+const port = 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
